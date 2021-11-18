d2d3c42a301fcab04986391a33dd9a0e98f6703b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-18 20:40:04 +0530

    Implemented authentication for the sigup module.

--- route/Signup.js
+++ route/Signup.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const signup = express();
 
+signup.use((req,res,next) => {
+    const {Authentication} = req.session;
+    if(Authentication || (req.path === '/' && req.method === 'POST')){
+        next();
+    }else{
+        res.status(403).json({
+            Success: false,
+            Message: 'Authentication required.'
+        })
+    }
+})
+
 signup.get('/', (req, res) => {
     res.json('signup module.')
+})
+
+signup.post('/', (req, res) => {
+    res.json('eererge');
 })
 
 module.exports = signup;
