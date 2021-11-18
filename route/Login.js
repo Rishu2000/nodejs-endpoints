9bafe4ab44ff2cde09617d85facbcf655b971dd2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-18 20:38:04 +0530

    Implemented authentication for the login module.

--- route/Login.js
+++ route/Login.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const login = express();
 
+login.use((req,res,next) => {
+    const {Authentication} = req.session;
+    if(Authentication || (req.path === '/' && req.method === 'POST')){
+        next();
+    }else{
+        res.status(403).json({
+            Success: false,
+            Message: 'Authentication required.'
+        })
+    }
+})
+
 login.get('/', (req, res) => {
     res.json('login module.')
+})
+
+login.post('/', (req, res) => {
+    res.json("efee");
 })
 
 module.exports = login;
