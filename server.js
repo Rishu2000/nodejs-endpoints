ddfc2287e2ceb7213d2d17eeffca8d918c2b580c
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-18 20:29:05 +0530

    Implemented express-session.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const port = 3004;
 const login = require('./route/Login');
 const signup = require('./route/Signup');
 
+app.use(session({
+    secret: 'nodejs-endpoints',
+    resave: false,
+    saveUninitialized: false
+}))
 app.use('/login',login);
 app.use('/signup',signup);
 app.get('/', (req, res) => {
