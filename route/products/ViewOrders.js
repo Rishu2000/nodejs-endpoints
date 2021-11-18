d6b6e374b84f43535230f6a776171cc365edf6e8
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-18 22:28:49 +0530

    Successfully Implemented add products for admin and view products for both.

--- route/products/ViewOrders.js
+++ route/products/ViewOrders.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const viewOrders = express();
+const {knex} = require('../../pg/connection');
 
 viewOrders.get('/', (req, res) => {
-    res.json("All orders.");
+    knex('products')
+        .then((rows) => {
+            res.json(rows);
+        })
+})
+
+viewOrders.post('/', (req, res) => {
+    if(req.session.Authentication.role === 'admin') {
+        const {name,id,price,description} = req.body;
+        knex('products')
+            .insert({name:name, id:id, price:price, description:description})
+            .then(() => {
+                res.status(201).json({
+                    Success: true,
+                    Message:`${name} has been Added.`
+                });
+            })
+    }else{
+        res.status(403).json({
+        Success: false,
+        Message:`You are not admin.`
+    });
+    }
 })
 
 module.exports = viewOrders;
